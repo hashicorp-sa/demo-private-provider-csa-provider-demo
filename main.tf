9ab3d09c7b8cf3cc3459521e7c28540042ca630a
jaredfholgate <jared.holgate@hashicorp.com>
2022-06-22 11:45:04 +0200

    Refactor for demo animal

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 terraform {
   required_providers {
     csa = {
-        source = "tfe.hashicorpdemo.net/provider-demo/csa"
+        source = "tfe.hashicorpdemo.net/provider-demo/demo"
+        
     }
   }
 }
 
 locals {
-  csa_members = [ "Jared Holgate", "Jan Repnak", "Colin Turney" ]
+  csa_members = [ "Bird", "Mammal", "Invertebrate", "Reptile", "Fish", "Amphibian" ]
 }
 
-resource "csa_specialism" "demo" {
+resource "demo_animal" "demo" {
     for_each = { for csa_member in local.csa_members : csa_member => csa_member }
     customer_success_architect = each.key
 }
 
-output "csa_specialisms" {
+output "demo_animals" {
   value = { for csa_member in local.csa_members : csa_member =>  csa_specialism.demo[csa_member].specialism }
 }
 
-output "csa_dates" {
+output "demo_dates" {
   value = { for csa_member in local.csa_members : csa_member =>  csa_specialism.demo[csa_member].date_configured }
 }
