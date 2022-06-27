d46aa0eb0fc19b061f754f32fdeeb9b289310fd8
Jared Holgate <jared.holgate@hashicorp.com>
2022-06-27 13:17:37 +0100

    Fix (#7)

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     demo = {
-      source = "tfe.hashicorpdemo.net/provider-demo/demo"
+      source = "tfe.hashicorpdemo.net/private-provider-demo/demo"
     }
   }
 }
