1f2885951bcd1a73d61b37c98a7e21be48ba3323
Jared Holgate <jared.holgate@hashicorp.com>
2022-08-03 16:06:03 +0100

    Fix for new org name (#8)
    * Fix
    
    * Update source
    
    * Remove experiment
    
    * Fix provider config

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ terraform {
   }
 }
 
+provider "demo" {
+  token = "sdsadsdafsdafsadf"
+  url = "https://demo.tfe.hashicorp.com"
+}
+
 resource "demo_animal" "demo" {
   for_each = { for animal in var.classes : animal => animal }
   class    = each.key
